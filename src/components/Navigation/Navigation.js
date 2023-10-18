c2852b4db0dbe9e6f09e8fd28fbad9f30499ed02
gen <regan035@gmail.com>
2023-10-17 21:35:47 -0500

    navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./Navigation.css";
 
-const Navigation ()={
+const Navigation = () => {
+  return (
+    <nav>
+      <p className="f3 link dim black underline pa3 pointer">Sign Out</p>
+    </nav>
+  );
+};
 
-}
-
-export default Navigation
+export default Navigation;
