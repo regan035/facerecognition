58a4bffab0c10ee40c22e611d86775ceaf8cde6f
gen <regan035@gmail.com>
2023-10-19 21:01:44 -0500

    update Rank

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import ParticlesBg from "particles-bg";
+import Rank from "./components/Rank/Rank";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <ParticlesBg type="cobweb" bg={true} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
     </div>
   );
